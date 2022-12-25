8c8e564be322f78469e3f9d40abf7ff912a26eea
deltaitech <deltaitech>
2022-12-25 10:07:25 +0200

    edit on call scripts in scripts file

--- resources/views/admin/components/icon.blade.php
+++ resources/views/admin/components/icon.blade.php
@@ -26,3 +26,17 @@
         </div>
     </div>
 @endif
+@section('scripts')
+    <script>
+        // Default options
+        IconPicker.Init({
+            // Required: You have to set the path of IconPicker JSON file to "jsonUrl" option. e.g. '/content/plugins/IconPicker/dist/iconpicker-1.5.0.json'
+            jsonUrl:"{{ asset('dashboard/js/iconpicker-1.5.0.json') }}",// Optional: Change the buttons or search placeholder text according to the language.
+            searchPlaceholder: 'Search Icon',
+            showAllButton: "{{__('words.show_all')}}",
+            cancelButton: "{{__('words.cancel')}}",
+            noResultsFound: "{{__('message.no_result')}}", // v1.5.0 and the next versionsborderRadius: '20px', // v1.5.0 and the next versions
+        });
+        IconPicker.Run('#GetIconPicker');
+    </script>
+    @endsection
