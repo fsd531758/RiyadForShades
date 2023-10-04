3d1f2077d08d6abc39b72d4adaa81bb751f65c87
Asmaa Marzouk <fsd531758@gmail.com>
2023-10-04 15:33:54 +0300

    Make dashboard ar only without locale in url

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
 Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
-    Route::prefix('dashboard')->group(function () {
+    Route::prefix('admin')->group(function () {
 
         //admin login
         Route::get('login', 'Auth\AuthController@login')->name('dashboard.login')->middleware('guest:admin');
